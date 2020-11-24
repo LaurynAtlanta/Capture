90edbba84d88cdfe15b81edb078c041fc8f4c8cc
“Lauryn <laurynmills111@gmail.com>
2020-11-24 17:47:07 -0500

    FAQ toggle animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,43 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 import {About} from '../styles';
+import Toggle from './toggle';
+import {AnimateSharedLayout} from 'framer-motion';
 
 const FaqSection = () => {
     return (  
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
+            <AnimateSharedLayout>
+            <Toggle title="How Do I Start?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, quis.</p>
+                    </div>
+            </Toggle>
+            <Toggle title="Daily Schedule?">
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, quis.</p>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule?</h4>
+            </Toggle>
+            <Toggle title="Different Payment Methods?">
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, quis.</p>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different Payment Methods?</h4>
+            </Toggle>
+            <Toggle title="What Products do you offer?">
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, quis.</p>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What Products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, quis.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            </Toggle>
+            </AnimateSharedLayout>
         </Faq>
     );
 }
@@ -50,6 +46,9 @@ const Faq = styled(About)`
     h2{
         padding-bottom: 2.5rem;
         font-weight: lighter;
+    }
+    h4{
+        cursor: pointer;
     }
     .faq-line{
         background: #cccccc;
